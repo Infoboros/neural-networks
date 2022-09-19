37a3cfc7fff5f6472a79b26f1e0bb84cb206274d
infoboros <pintastd@gmail.com>
2022-09-19 22:22:50 +0300

    [lab1] bad defence recognize function

--- lab1/src/models/recognize/index.js
+++ lab1/src/models/recognize/index.js
@@ -5,6 +5,10 @@ export function average(nums) {
     return nums.reduce((a, b) => (a + b)) / nums.length;
 }
 
+export function divider(a, b, part) {
+    return (a + part * b) / (1. + part)
+}
+
 export const recognizeFunctions = [
     {
         id: 1,
@@ -23,6 +27,19 @@ export const recognizeFunctions = [
                 : 0,
         name: 'Бинарная',
         getFrontier: (S1, S0) => average([S1, S0])
+    },
+    {
+        id: 3,
+        recognize: (xs, ws, S1, S0) => {
+            const S = getS(xs, ws)
+            const T =
+                -Math.log((1 - 0.9) / 0.9)
+                /
+                average([divider(S1, S0, 1. / 3.), divider(S1, S0, 2. / 3.)])
+            return 1/ (1 + Math.exp(-T*S))
+        },
+        name: 'Бинарная Сигмоидальная',
+        getFrontier: (S1, S0) => `Q1=${divider(S0, S1, 1. / 3.)};Q2=${divider(S0, S1, 2. / 3.)}`
     }
 ]
 
